13910c4224c40663c7920721cde6f1c8d28ecfbb
namtv <thannam27062001@gmail.com>
2022-07-29 13:09:49 +0700

    update return message

--- VSII/src/main/java/com/example/vsii/controller/ClaimController.java
+++ VSII/src/main/java/com/example/vsii/controller/ClaimController.java
@@ -3,6 +3,8 @@ package com.example.vsii.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,18 +30,22 @@ public class ClaimController {
 	private MessageConstant messageConstant;
 	
 	@PostMapping("/add")
-	public MessageUtils addUser(@Valid @RequestBody ClaimModel model, BindingResult result, Model model2) {
+	public ResponseEntity<MessageUtils> addUser(@Valid @RequestBody ClaimModel model, BindingResult result, Model model2) {
 		ClaimEntity ca = this.serviceClaim.findByApp(model.getApplicationNumber());
 		if (result.hasErrors() == true) {
 			model2.addAttribute("massage", "dữ liệu không hợp lệ");
-			return new MessageUtils(messageConstant.INVALID_ERRO,messageConstant.INVALID_ERRO_CODE);
+			//return new MessageUtils(messageConstant.INVALID_ERRO,messageConstant.INVALID_ERRO_CODE);
+			return ResponseEntity.status(HttpStatus.OK).body(new MessageUtils(messageConstant.INVALID_ERRO,messageConstant.INVALID_ERRO_CODE));
+			
 		}
 		if (ca != null) {
 			serviceClaim.update(model);
-			return new MessageUtils(messageConstant.SUCCESSFUL_INSERT,messageConstant.SUCCESSFUL_CODE);
-		}
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new MessageUtils(messageConstant.SUCCESSFUL_UPDATE,messageConstant.SUCCESSFUL_CODE));
+	}
 		serviceClaim.insert(model);
-		return new MessageUtils(messageConstant.SUCCESSFUL_UPDATE,messageConstant.SUCCESSFUL_CODE);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(new MessageUtils(messageConstant.SUCCESSFUL_INSERT,messageConstant.SUCCESSFUL_CODE));
 
 	}
 
