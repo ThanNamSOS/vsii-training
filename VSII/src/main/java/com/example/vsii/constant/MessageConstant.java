834ca3b6edb8a68ea7aba2429d332aee80feafcf
namtv <thannam27062001@gmail.com>
2022-07-29 13:12:00 +0700

    update Constant message

--- VSII/src/main/java/com/example/vsii/constant/MessageConstant.java
+++ VSII/src/main/java/com/example/vsii/constant/MessageConstant.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageConstant {
 	public static final String INVALID_ERRO = "Invalid error";
-	public static final String SUCCESSFUL_INSERT = "Successful";
-	public static final String SUCCESSFUL_UPDATE = "Successful";
+	public static final String SUCCESSFUL_INSERT = "Successful insert";
+	public static final String SUCCESSFUL_UPDATE = "Successful update";
 	
 	public static final String INVALID_ERRO_CODE = "400";
 	public static final String SUCCESSFUL_CODE = "200";
--- VSII/src/main/java/com/example/vsii/controller/ClaimController.java
+++ VSII/src/main/java/com/example/vsii/controller/ClaimController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.vsii.constant.MessageConstant;
 import com.example.vsii.entities.ClaimEntity;
 import com.example.vsii.model.ClaimModel;
-import com.example.vsii.repository.ClaimRepository;
 import com.example.vsii.services.IService;
 import com.example.vsii.utils.MessageUtils;
 
@@ -27,25 +26,25 @@ public class ClaimController {
 	private IService serviceClaim;
 
 	@Autowired
-	private MessageConstant messageConstant;
-	
+	private MessageConstant message;
+
 	@PostMapping("/add")
-	public ResponseEntity<MessageUtils> addUser(@Valid @RequestBody ClaimModel model, BindingResult result, Model model2) {
+	public ResponseEntity<MessageUtils> addUser(@Valid @RequestBody ClaimModel model, BindingResult result,
+			Model model2) {
 		ClaimEntity ca = this.serviceClaim.findByApp(model.getApplicationNumber());
 		if (result.hasErrors() == true) {
 			model2.addAttribute("massage", "dữ liệu không hợp lệ");
-			//return new MessageUtils(messageConstant.INVALID_ERRO,messageConstant.INVALID_ERRO_CODE);
-			return ResponseEntity.status(HttpStatus.OK).body(new MessageUtils(messageConstant.INVALID_ERRO,messageConstant.INVALID_ERRO_CODE));
-			
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(new MessageUtils(message.INVALID_ERRO, message.INVALID_ERRO_CODE));
 		}
 		if (ca != null) {
 			serviceClaim.update(model);
 			return ResponseEntity.status(HttpStatus.OK)
-					.body(new MessageUtils(messageConstant.SUCCESSFUL_UPDATE,messageConstant.SUCCESSFUL_CODE));
-	}
+					.body(new MessageUtils(message.SUCCESSFUL_UPDATE, message.SUCCESSFUL_CODE));
+		}
 		serviceClaim.insert(model);
 		return ResponseEntity.status(HttpStatus.OK)
-				.body(new MessageUtils(messageConstant.SUCCESSFUL_INSERT,messageConstant.SUCCESSFUL_CODE));
+				.body(new MessageUtils(message.SUCCESSFUL_INSERT, message.SUCCESSFUL_CODE));
 
 	}
 
